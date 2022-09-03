1cd61cbe723bf2b7bf883f8c01454fd04f6a53d9
kostya <kostizavali@gmail.com>
2022-09-03 15:57:19 +0300

    tasked

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,45 +1,25 @@
-const refs = {
-  form: document.querySelector('.feedback-form'),
-  textarea: document.querySelector('.feedback-form textarea'),
-  input: document.querySelector('.feedback-form input'),
+const throttle = require('lodash.throttle');
+const elem = document.querySelector('.feedback-form');
+const storage = localStorage.getItem('feedback-form-state');
+
+if (storage) {
+  document.querySelector("[type='email']").value = JSON.parse(storage).email;
+  document.querySelector("[name='message']").value =
+    JSON.parse(storage).message;
+}
+
+const handleInput = event => {
+  localStorage.setItem(
+    'feedback-form-state',
+    JSON.stringify({ email: elem.email.value, message: elem.message.value })
+  );
 };
+elem.addEventListener('input', throttle(handleInput, 500));
 
-const STORAGE_KEY_EMAIL = 'feedback-form-email';
-const STORAGE_KEY_STATE = 'feedback-form-state';
-refs.form.addEventListener('submit', onFormSubmit);
-refs.textarea.addEventListener('input', onTextareaInput);
-refs.input.addEventListener('input', onTextareaInputEmail);
-
-function onFormSubmit(e) {
-  e.preventDefault();
-
-  console.log('Send message');
-  populateMessageInput();
-  populateMessageOutput();
-  e.currentTarget.reset();
-
-  localStorage.removeItem(STORAGE_KEY_STATE);
-  localStorage.removeItem(STORAGE_KEY_EMAIL);
-}
-function onTextareaInput(e) {
-  const message = e.currentTarget.value;
-  localStorage.setItem(STORAGE_KEY_STATE, message);
-}
-function onTextareaInputEmail(e) {
-  const email = e.currentTarget.value;
-  localStorage.setItem(STORAGE_KEY_EMAIL, email);
-}
-function populateMessageOutput() {
-  const saveMesagge = localStorage.getItem(STORAGE_KEY_STATE);
-  if (saveMesagge) {
-    console.log('message:', saveMesagge);
-    refs.textarea.value = saveMesagge;
-  }
-}
-function populateMessageInput() {
-  const saveEmail = localStorage.getItem(STORAGE_KEY_EMAIL);
-  if (saveEmail) {
-    console.log('email:', saveEmail);
-    refs.input.value = saveEmail;
-  }
-}
+const handleSubmit = event => {
+  event.preventDefault();
+  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+  localStorage.clear();
+  elem.reset();
+};
+elem.addEventListener('submit', handleSubmit);
